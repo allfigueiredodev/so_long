92a5b1396f1693b2643dbed1363dae86af514aa3
Allesson Figueiredo <allfigueiredo@outlook.com>
2023-08-24 19:13:29 -0300

    last commit | heap 0

--- is_trapped.c
+++ is_trapped.c
@@ -6,7 +6,7 @@
 /*   By: aperis-p <aperis-p@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 13:42:02 by aperis-p          #+#    #+#             */
-/*   Updated: 2023/08/24 18:59:46 by aperis-p         ###   ########.fr       */
+/*   Updated: 2023/08/24 19:03:36 by aperis-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,9 @@ int	is_trapped(t_wdata *wdata, char **map)
 	t_point	size;
 	t_point	player;
 	char	**map_copy;
-	int		i;
-	int		j;
 
 	size.x = wdata->mapinfo.s_width / 47;
 	size.y = wdata->mapinfo.s_height / 47;
-	i = 0;
-	j = 0;
 	clone_matrix(&map_copy, map, size.x, size.y);
 	set_current(&player.x, &player.y, map_copy);
 	flood_fill(map_copy, size, player);
